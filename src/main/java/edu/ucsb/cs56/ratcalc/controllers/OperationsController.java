4d7d7f2a4dae1bf03e4587c4b56b726e23c762f9
Lele521 <tengyue@umail.ucsb.edu>
2020-01-29 00:32:39 -0800

    th - added controller method to show divide form

--- src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/controllers/OperationsController.java
@@ -129,4 +129,12 @@ public class OperationsController {
         return "operations/multiply";
     }
 
+    @GetMapping("/divide")
+    public String getDivide(Model model) {
+        RatCalcForm ratCalcForm = new RatCalcForm();
+        ratCalcForm.setOp("/");
+        model.addAttribute("ratCalcForm", ratCalcForm);
+        return "operations/divide";
+    }
+
 }
